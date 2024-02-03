0cfaabaf8566a8fa6e4d5257962f3d8d57cf655b
M K <mkpro118@gmail.com>
2024-02-03 00:33:38 -0600

    Fuzz Testing!

--- Project 1/P1.java
+++ Project 1/P1.java
@@ -2,8 +2,13 @@ import java.io.ByteArrayOutputStream;
 import java.io.PrintStream;
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Set;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
+
+import static java.util.regex.Pattern.compile;
+import static java.lang.Math.random;
 
 /**
  * @author Mrigank Kumar
@@ -13,8 +18,11 @@ import java.util.regex.Pattern;
 public final class P1 {
     private final static String TYPE = "int";
     private final static Sym SYM = new Sym(TYPE);
-    private final static int N_TESTS = 92;
+    private final static int N_TESTS = 100;
     private final static PrintStream defaultPrintStream = System.out;
+    private final static int MAX_STRING_LENGTH = 1 << 4;
+    private final static int MIN_FUZZ_ITERS = 1 << 7;
+    private final static int FUZZ_ITERS_RANGE = (1 << 10) - MIN_FUZZ_ITERS;
 
     private int score;
     private int testsRun;
@@ -42,6 +50,70 @@ public final class P1 {
 
     private final static void resetPrintStream() {
         System.setOut(defaultPrintStream);
+    }
+
+    private final void checkSymbolString(String[][] symbols,
+        HashMap<String, String> testMap, String content) throws Exception {
+        String[] pairs = content.split(",");
+
+        testsRun++;
+        if (pairs.length != symbols.length) {
+            printError("Mismatched number of symbols.\n"
+                + "Expected: " + symbols.length + " symbols, " + testMap
+                + "\nActual:   " + pairs.length + " symbols, {"
+                + content+ "}");
+            return;
+        }
+        score++;
+
+        for (String pair : pairs) {
+            String[] data = pair.split("=");
+
+            testsRun++;
+            if (data.length != 2) {
+                printError("Invalid format!\n"
+                    + "Expected format is \"name=type\"\n"
+                    + "Actual contents: \"" + pair + "\"");
+                return;
+            }
+            score++;
+
+            String key = data[0].trim();
+            String value = data[1].trim();
+
+            testsRun++;
+            if (!testMap.containsKey(key)) {
+                printError("Unexpected symbol!\n"
+                    + "Found unexpected symbol \"" + key + "\"\n"
+                    + "Expected symbols are " + testMap);
+                return;
+            }
+            score++;
+
+            testsRun++;
+            String expectedValue = testMap.get(key);
+            if (!expectedValue.equals(value)) {
+                printError("Incorrect mapping!\n"
+                    + "Expected symbol \"" + key + "\" to have type \""
+                    + expectedValue + "\"\n"
+                    + "Found type \"" + value + "\"");
+                return;
+            }
+            score++;
+        }
+    }
+
+    private final static String generateRandomString() {
+        final int length = (int) (random() * MAX_STRING_LENGTH) + 1;
+
+        String random = "";
+
+        for (int i = 0; i < length; i++) {
+            char start = (random() > 0.5 ? 'a' : 'A');
+            random += (char) (start + (char)(random() * 26));
+        }
+
+        return random;
     }
 
     public final void testSymConstructor() throws Exception {
@@ -444,61 +516,10 @@ public final class P1 {
         }
     }
 
-    private final void checkSymbolString(String[][] symbols,
-        HashMap<String, String> testMap, String content) throws Exception {
-        String[] pairs = content.split(",");
-
-        testsRun++;
-        if (pairs.length != symbols.length) {
-            printError("Mismatched number of symbols.\n"
-                + "Expected: " + symbols.length + " symbols, " + testMap
-                + "\nActual:   " + pairs.length + " symbols, {"
-                + content+ "}");
-            return;
-        }
-        score++;
-
-        for (String pair : pairs) {
-            String[] data = pair.split("=");
-
-            testsRun++;
-            if (data.length != 2) {
-                printError("Invalid format!\n"
-                    + "Expected format is \"name=type\"\n"
-                    + "Actual contents: \"" + pair + "\"");
-                return;
-            }
-            score++;
-
-            String key = data[0].trim();
-            String value = data[1].trim();
-
-            testsRun++;
-            if (!testMap.containsKey(key)) {
-                printError("Unexpected symbol!\n"
-                    + "Found unexpected symbol \"" + key + "\"\n"
-                    + "Expected symbols are " + testMap);
-                return;
-            }
-            score++;
-
-            testsRun++;
-            String expectedValue = testMap.get(key);
-            if (!expectedValue.equals(value)) {
-                printError("Incorrect mapping!\n"
-                    + "Expected symbol \"" + key + "\" to have type \""
-                    + expectedValue + "\"\n"
-                    + "Found type \"" + value + "\"");
-                return;
-            }
-            score++;
-        }
-    }
-
     public final void testSymTablePrint() throws Exception {
         currentTest = "SymTable Print";
 
-        Pattern pattern = Pattern.compile("\\+{4}\\s*SYMBOL\\s*TABLE\\s*\\"
+        Pattern pattern = compile("\\+{4}\\s*SYMBOL\\s*TABLE\\s*\\"
                 + "{(.*)\\}\\s*\\+{4}\\s*END\\sTABLE");
 
         // 1. Symbol Table with global scope, no declarations
@@ -636,7 +657,7 @@ public final class P1 {
                 expectedOutput = testMap + "\n" + expectedOutput;
             }
 
-            Pattern multiScopePattern = Pattern.compile("\\+{4}\\s*SYMBOL\\s*"
+            Pattern multiScopePattern = compile("\\+{4}\\s*SYMBOL\\s*"
                 +"TABLE\\s*((\\{.*\\}\\s*){" + numberOfScopes
                 + "})\\s*\\+{4}\\s*END\\s*TABLE");
 
@@ -686,6 +707,248 @@ public final class P1 {
         }
     }
 
+    public final void testSymConstructorFuzz() throws Exception {
+        currentTest = "Sym Constructor Fuzz";
+
+        final int iters = (int) (random() * FUZZ_ITERS_RANGE) + MIN_FUZZ_ITERS;
+
+        testsRun++;
+        for (int i = 0; i < iters; i++) {
+            new Sym(generateRandomString());
+        }
+        score++;
+    }
+
+    public final void testSymAccessorFuzz() throws Exception {
+        currentTest = "Sym Accessor Fuzz";
+
+        final int iters = (int) (random() * FUZZ_ITERS_RANGE) + MIN_FUZZ_ITERS;
+
+        testsRun++;
+        for (int i = 0; i < iters; i++) {
+            String randomType = generateRandomString();
+            String actual = (new Sym(randomType)).getSym();
+            if (!randomType.equals(actual)) {
+                printError("getSym() returned the wrong sym\n"
+                    + "Expected: " + randomType + "\n"
+                    + "Actual:   " + actual);
+                return;
+            }
+        }
+        score++;
+    }
+
+    public final void testSymToStringFuzz() throws Exception {
+        currentTest = "Sym Accessor Fuzz";
+
+        final int iters = (int) (random() * FUZZ_ITERS_RANGE) + MIN_FUZZ_ITERS;
+
+        testsRun++;
+        for (int i = 0; i < iters; i++) {
+            String randomType = generateRandomString();
+            String actual = (new Sym(randomType)).toString();
+            if (!randomType.equals(actual)) {
+                printError("getSym() returned the wrong sym\n"
+                    + "Expected: " + randomType + "\n"
+                    + "Actual:   " + actual);
+                return;
+            }
+        }
+        score++;
+    }
+
+    public final void testSymTableAddDeclFuzz() throws Exception {
+        currentTest = "SymTable Add Declaration Fuzz";
+
+        SymTable table = new SymTable();
+
+        final int iters = (int) (random() * FUZZ_ITERS_RANGE) + MIN_FUZZ_ITERS;
+
+        HashSet<String> symbolSet = new HashSet<>();
+
+        testsRun++;
+        while (symbolSet.size() < iters) {
+            String name;
+            do {
+                name = generateRandomString();
+            }  while (symbolSet.contains(name));
+            symbolSet.add(name);
+            table.addDecl(name, new Sym(name));
+        }
+        score++;
+
+        String[] symbols = new String[iters];
+        symbols = symbolSet.toArray(symbols);
+
+        // Duplicates
+        final int dups = (int) (random() * iters) + MIN_FUZZ_ITERS;
+
+        testsRun++;
+        for (int i = 0; i < dups; i++) {
+            final int index = (int) (random() * iters);
+            try {
+                table.addDecl(symbols[index], new Sym(symbols[index]));
+                printError("Expected DuplicateSymNameException");
+            } catch (DuplicateSymNameException e) {}
+        }
+        score++;
+    }
+
+    public final void testSymTableAddRemoveScopeFuzz() throws Exception {
+        currentTest = "SymTable Add/Remove Fuzz";
+
+        final int iters = (int) (random() * FUZZ_ITERS_RANGE) + MIN_FUZZ_ITERS;
+
+        SymTable table = new SymTable();
+        int stack = 1;
+
+        testsRun++;
+        for (int i = 0; i < iters; i++) {
+            if (random() > 0.5) {
+                try {
+                    table.removeScope();
+                    stack--;
+                    if (stack < 0) {
+                        printError("Expected EmptySymTableException");
+                        return;
+                    }
+                } catch (EmptySymTableException e) {
+                    if (stack > 0) {
+                        throw e;
+                    }
+                }
+            } else {
+                table.addScope();
+                stack++;
+            }
+
+        }
+        score++;
+    }
+
+    public final void testSymTableLookupLocalFuzz() throws Exception {
+        currentTest = "SymTable Lookup Local Fuzz";
+
+        final int numberOfScopes = (int) (random() * 75) + 25;
+        final int numberOfSymbols = (int) (random() * 75) + 25;
+
+        String[] symbols = new String[numberOfSymbols];
+
+        SymTable table = new SymTable();
+
+        for (int i = 0; i < numberOfSymbols; i++) {
+            symbols[i] = generateRandomString();
+        }
+
+        testsRun++;
+        for (int i = 0; i < numberOfScopes - 1; i++) {
+            table.addScope();
+
+            final int nSyms = (int) (random() * 15) + 5;
+            HashMap<String, String> usedSymbols = new HashMap<>();
+
+            for (int j = 0; j < nSyms; j++) {
+
+                String name = symbols[(int) (random() * numberOfSymbols)];
+                String type = symbols[(int) (random() * numberOfSymbols)];
+
+                if (usedSymbols.containsKey(name)) {
+                    continue;
+                }
+
+                usedSymbols.put(name, type);
+                table.addDecl(name, new Sym(type));
+            }
+
+            for (String symbol: symbols) {
+                String type = usedSymbols.get(symbol);
+
+                if (type != null) {
+                    String actual = table.lookupLocal(symbol).toString();
+                    if (!type.equals(actual)) {
+                        printError("Wrong type found!\n"
+                            + "Expected: " + type + "\n"
+                            + "Actual:   " + actual);
+                        return;
+                    }
+                } else {
+                    if (table.lookupLocal(symbol) != null) {
+                        printError("Unexpected symbol found in local scope!\n"
+                            + "Symbol " + symbol + " should not exist.\n"
+                            + "Symbols in current scope are: "
+                            + usedSymbols.keySet());
+                        return;
+                    }
+                }
+            }
+        }
+        score++;
+    }
+
+    public final void testSymTableLookupGlobalFuzz() throws Exception {
+        currentTest = "SymTable Lookup Local Fuzz";
+
+        final int iters = (int) (random() * FUZZ_ITERS_RANGE) + MIN_FUZZ_ITERS;
+        final int numberOfSymbols = (int) (random() * 75) + 25;
+
+        String[] symbols = new String[numberOfSymbols];
+
+        SymTable table = new SymTable();
+
+        for (int i = 0; i < numberOfSymbols; i++) {
+            symbols[i] = generateRandomString();
+        }
+
+        HashMap<String, String> usedSymbols = new HashMap<>();
+
+        for (int i = 0; i < iters; i++) {
+            if (random() > 0.85) {
+                table.addScope();
+            }
+
+            String name;
+
+            do {
+                name = generateRandomString();
+            } while (usedSymbols.containsKey(name));
+
+            String type = generateRandomString();
+
+            usedSymbols.put(name, type);
+
+            table.addDecl(name, new Sym(type));
+        }
+
+        testsRun++;
+        for (String name: usedSymbols.keySet()) {
+            String type = usedSymbols.get(name);
+            Sym sym = table.lookupGlobal(name);
+
+            if (sym == null || !type.equals(sym.toString())) {
+                printError("Sym not found!\n"
+                    + "Expected: " + name + "=" + type + "\n"
+                    + "Actual:   " + name + "=null");
+                return;
+            }
+        }
+
+        for (int i = 0; i < iters; i++) {
+            String randomName;
+
+            do {
+                randomName = generateRandomString();
+            } while(usedSymbols.containsKey(randomName));
+
+            if (table.lookupGlobal(randomName) != null) {
+                printError("Unexpected symbol found!\n"
+                    + "Found symbol " + randomName + "\n"
+                    + "List of symbols across all scopes are: " + usedSymbols);
+            }
+        }
+
+        score++;
+    }
+
     public final void printScore() {
         if (score > testsRun) {
             System.out.println("INVALID TESTER!!! Score is greater than the "
@@ -714,15 +977,23 @@ public final class P1 {
         P1 tester = new P1();
 
         try {
-            tester.testSymConstructor();
-            tester.testSymAccessor();
-            tester.testSymToString();
-            tester.testSymTableConstructor();
-            tester.testSymTableAddRemoveScope();
-            tester.testSymTableAddDecl();
-            tester.testSymTableLookupLocal();
-            tester.testSymTableLookupGlobal();
-            tester.testSymTablePrint();
+                                                      // |  # Points  | Total |
+            tester.testSymConstructor();              // |  2 points  |    2  |
+            tester.testSymAccessor();                 // |  2 points  |    4  |
+            tester.testSymToString();                 // |  2 points  |    6  |
+            tester.testSymTableConstructor();         // |  1 point   |    7  |
+            tester.testSymTableAddRemoveScope();      // |  6 points  |   13  |
+            tester.testSymTableAddDecl();             // |  9 points  |   22  |
+            tester.testSymTableLookupLocal();         // |  4 points  |   26  |
+            tester.testSymTableLookupGlobal();        // |  9 points  |   35  |
+            tester.testSymTablePrint();               // | 57 points  |   92  |
+            tester.testSymConstructorFuzz();          // |  1 point   |   93  |
+            tester.testSymToStringFuzz();             // |  1 point   |   94  |
+            tester.testSymAccessorFuzz();             // |  1 point   |   95  |
+            tester.testSymTableAddRemoveScopeFuzz();  // |  1 point   |   96  |
+            tester.testSymTableLookupLocalFuzz();     // |  1 point   |   97  |
+            tester.testSymTableLookupGlobalFuzz();    // |  1 point   |   98  |
+            tester.testSymTableAddDeclFuzz();         // |  2 points  |  100  |
         } catch (Exception e) {
             tester.printError("(Unexpected Exception)");
             e.printStackTrace();
